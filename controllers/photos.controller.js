fd387a07220fb8a8079abef65816266ca96bd80b
Konrad Brożyna <kondzio3002@gmail.com>
2023-03-09 22:15:06 +0100

    Validation form

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -10,10 +10,47 @@ exports.add = async (req, res) => {
 
     if(title && author && email && file) { // if fields are not empty...
 
+      const authorPattern = new RegExp(
+        /(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,
+        "g"
+      );
+      const authorMatched = author.match(authorPattern).join('');
+      if(authorMatched.length < author.length) {
+        res.status(500).json({ message: 'Invalid characters...' });
+        throw new Error('Invalid characters')
+      };
+
+      const emailPattern = new RegExp(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "g"
+      );
+      const emailMatched = email.match(emailPattern).join('');
+      if(emailMatched.length < email.length) {
+        res.status(500).json({ message: 'Invalid characters...' });
+        throw new Error('Invalid characters')
+      };
+
+      const titlePattern = new RegExp(
+        /(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,
+        "g"
+      );
+      const titleMatched = title.match(titlePattern).join('');
+      if(titleMatched.length < title.length) {
+        res.status(500).json({ message: 'Invalid characters...' });
+        throw new Error('Invalid characters...')
+      };
+
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      const fileExt = fileName.split('.').slice(-1)[0];
+
+      if((fileExt[1] === '.gif' || '.jpg' || '.png') && (title.length <= 25) && (author.length <= 50)) {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        res.status(500).json({ message: 'Wrong input!' });
+        throw new Error('Wrong input!');
+      }
 
     } else {
       throw new Error('Wrong input!');
